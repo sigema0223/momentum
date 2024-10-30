867d2cb21ff1945c8acb856ccedffdbd32ddba7f
sigema0223 <sigema0223@gmail.com>
2024-10-30 15:29:58 +0000

    Let's start

---
+++ README.md
@@ -0,0 +1,3 @@
+## Basic Tutorial for JavaScript
+
+Cloning momentum
---
+++ app.js
@@ -0,0 +1,7 @@
+const num1 = 5, num2 = 2;
+const myName = "Hyunwoo"
+
+console.log(num1/num2)
+console.log(num1+num2)
+console.log(num1*num2)
+console.log("Hello " + myName)
