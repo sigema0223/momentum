e5774840a76cb2cf18428f28b2390a83d251bb95
sigema0223 <sigema0223@gmail.com>
2024-10-30 15:49:09 +0000

    Let's go

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const num1 = 5, num2 = 2;
-const myName = "Hyunwoo"
+let myName = "Hyunwoo" // let can be changed, but const can't
+let something;
 
 console.log(num1/num2)
 console.log(num1+num2)
 console.log(num1*num2)
 console.log("Hello " + myName)
+console.log(something)
+
+myName = "Hyun"
+console.log("Your new name is " + myName)
